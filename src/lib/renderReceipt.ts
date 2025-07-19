cf5c012a798515c0221d3604d74ec51844c5d714
naamahrgl <naama.hargil@gmail.com>
2025-07-19 20:47:32 +0300

    receipt

--- src/lib/renderReceipt.ts
+++ src/lib/renderReceipt.ts
@@ -3,12 +3,26 @@ import fs from 'fs/promises';
 import path from 'path';
 import type { OrderData } from './orders';
 
+import { readFileSync } from 'fs';
+
+
+
+
 export async function renderReceiptFromOrder({ order, serial }: { order: OrderData, serial: number }) {
     
   const templatePath = path.resolve('src/templates/receiptTemplate.html');
   const template = await fs.readFile(templatePath, 'utf-8');
+const paymentsRows: string[] = [];
 
   const date = new Date().toLocaleDateString('he-IL');
+const deliveryDate = new Date(order.deliveryDate);
+deliveryDate.setHours(deliveryDate.getHours() + 3);
+
+const deliveryDateFormatted = deliveryDate.toLocaleDateString('he-IL', {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit'
+});
 
   const itemsHtml = order.items.map(item => `
     <tr>
@@ -19,16 +33,43 @@ export async function renderReceiptFromOrder({ order, serial }: { order: OrderDa
     </tr>
   `).join('');
 
-  const paymentsHtml = `
+    const deliveryHtml = `
+    <tr>
+      <td style="padding: 8px;">${order.deliveryMethod} | ${deliveryDateFormatted} </td>
+      <td style="padding: 8px;">1</td>
+      <td style="padding: 8px;">₪${order.deliveryFee}</td>
+      <td style="padding: 8px;">₪${order.deliveryFee}</td>
+    </tr>
+  `;
+  const newitemsHtml = itemsHtml + deliveryHtml
+
+if (order.couponAmount > 0) {
+  paymentsRows.push(`
+    <tr>
+      <td style="padding: 8px;">Coupon</td>
+      <td style="padding: 8px;">${order.couponCode}</td>
+      <td style="padding: 8px;">${date}</td>
+      <td style="padding: 8px;">₪${order.couponAmount}-</td>
+    </tr>
+  `);
+}
+
+// תשלום רגיל
+if (order.amountToPay > 0) {
+  paymentsRows.push(`
     <tr>
       <td style="padding: 8px;">Allpay</td>
-      <td style="padding: 8px;">${order.deliveryMethod}</td>
+      <td style="padding: 8px;">${order.paymentMethod || ''}</td>
       <td style="padding: 8px;">${date}</td>
       <td style="padding: 8px;">₪${order.amountToPay}</td>
     </tr>
-  `;
+  `);
+}
+
+const paymentsHtml = paymentsRows.join('');
 
   const totalItems = order.items.reduce((sum, i) => sum + (i.price * i.quantity), 0);
+  const finalTotal = totalItems + order.deliveryFee
   const totalPayments = order.amountToPay;
 
   return template
@@ -38,9 +79,9 @@ export async function renderReceiptFromOrder({ order, serial }: { order: OrderDa
     .replace(/{{customer.address}}/g, order.customerAddress || '')
     .replace(/{{serial}}/g, serial.toString())
     .replace(/{{date}}/g, date)
-    .replace(/{{items}}/g, itemsHtml)
+    .replace(/{{items}}/g, newitemsHtml )
     .replace(/{{payments}}/g, paymentsHtml)
-    .replace(/{{totalItems}}/g, totalItems.toString())
+    .replace(/{{totalItems}}/g, finalTotal.toString())
     .replace(/{{totalPayments}}/g, totalPayments.toString());
 }
 
