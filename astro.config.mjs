6ba2c3893043236d55156c13b5fdcfcb9de02b7d
naamahrgl <naama.hargil@gmail.com>
2026-02-24 21:30:57 +0200

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import react from '@astrojs/react';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), sitemap()],
    output: "server",
   adapter: netlify(),
   vite: {
